caa40c6c9406200a9e58c2c21694bdfb0eb5298f
Danila Leontyev <danila.leontyev.web@gmail.com>
2017-06-17 01:21:43 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@
  * Created by Donsh on 6/14/2017.
  */
 
-export function counterSiblings(x) {
-    //тут писать код
-    return x;
+export function palindrome(str) {
+    const reg = [
+        /[,]/g,
+        /[ ]/g,
+        /[.]/g,
+        /[_]/g,
+        /[/]/g,
+        /-/g,
+        /[(]/g,
+        /[)]/g,
+    ];
+
+    let s = str;
+    for(let i = 0; i <= reg.length - 1; i++ ){
+        s = s.toLowerCase().replace( reg[i], '');
+    }
+
+    let arr = s.split(''); //разбиваем строк на массив по отсутсвию символа
+    let reverseString = arr.reverse().join('');
+
+
+    console.log(s);
+    console.log(reverseString);
+    return s === reverseString;
 }
--- test/test.js
+++ test/test.js
@@ -2,12 +2,33 @@
  * Created by Donsh on 6/14/2017.
  */
 import assert from 'assert';
-import {counterSiblings} from '../src/index';
+import {palindrome} from '../src/index';
 
 
 //тут пишем тесты
-describe('counterSiblings', function () {
-    it('should return 1', function () {
-        assert.equal(counterSiblings(1), 1)
+describe('checkPalindrom', function () {
+    it('should return true', function () {
+        assert.equal(palindrome("eye"), true)
+    });
+    it('should return true', function () {
+        assert.equal(palindrome("_eye"), true)
+    });
+    it('should return true', function () {
+        assert.equal(palindrome("Race car"), true)
+    });
+    it('should return true', function () {
+        assert.equal(palindrome("A man, a plan, a canal. Panama"), true)
+    });
+    it('should return false', function () {
+        assert.equal(palindrome("not a palindrome"), false);
+    });
+    it('should return false', function () {
+        assert.equal(palindrome("five|\_/|four"), false)
+    });
+    it('should return false', function () {
+        assert.equal(palindrome("not-a p-alindrome"), false)
+    });
+    it('should return false', function () {
+        assert.equal(palindrome("0_0 (: /-\ :) 0-0"), true)
     });
 });
